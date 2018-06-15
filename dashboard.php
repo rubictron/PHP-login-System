d1e2eb5ddd68ee85e8915699117ec2146f971efa
rubictron <p.asithasandakelum@gmail.com>
2018-06-15 15:06:56 +0530

    modified:   dashboard.php

--- dashboard.php
+++ dashboard.php
@@ -15,26 +15,51 @@ include "logincheck.php";
     <link rel="stylesheet" href="fw/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/styles.css">
     <script type="text/javascript" src="fw/js/jquery-3.3.1.min.js"></script>
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
 
     <title>Log in</title>
 </head>
 
 <body class="container">
+<main>
 <div>
     <?php
+    echo "You are login as ";
     echo $_SESSION['username'];
 
     ?>
 
 </div>
-<div><button type="button" id="btnlogout" > LOG OUT </button>
+<div><button class="btn btn-primary" type="button" id="btnlogout" > LOG OUT </button>
 </div>
 
 <div>
 
 
-    This template is created by Asitha Sandakalum (rubictron) please dont remove the authorization.
+    This template is created by Asitha Sandakalum (rubictron) <br>
+    please dont infringe CopyRight.
 </div>
+</main>
+
+<footer id="footer">
+
+    <div> <i class="fa fa-copyright" aria-hidden="true"></i>&nbsp CopyRright &nbsp&nbsp Asitha Sandakalum (Rubictron)<br>
+        <a href="https://github.com/rubictron?tab=repositories">
+            <i class="fab fa-internet-explorer"></i>&nbsp www.asithasandakalum.tk
+        </a>&nbsp&nbsp&nbsp&nbsp&nbsp
+        <a href="https://rubictronsandakalum.blogspot.com">
+            <i class="fab fa-blogger-b"></i>&nbsp www.rubictronsandakalum.blogspot.com
+        </a>&nbsp&nbsp&nbsp&nbsp&nbsp
+        <a href="https://github.com/rubictron?tab=repositories">
+            <i class="fab fa-github-square"></i>&nbsp Git hub(rubictron)
+        </a>
+    </div>
+
+
+    </div>
+
+</footer>
 
 </body>
 <script>
--- registor.php
+++ registor.php
@@ -14,6 +14,8 @@
     <script src="fw/js/jquery-3.3.1.min.js"></script>
     <link rel="stylesheet" href="fw/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/styles.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
+
     <title>Registor</title>
 </head>
 
@@ -73,6 +75,25 @@
     </div>
 
 </main>
+
+<footer id="footer">
+
+    <div> <i class="fa fa-copyright" aria-hidden="true"></i>&nbsp CopyRright &nbsp&nbsp Asitha Sandakalum (Rubictron)<br>
+        <a href="https://github.com/rubictron?tab=repositories">
+            <i class="fab fa-internet-explorer"></i>&nbsp www.asithasandakalum.tk
+        </a>&nbsp&nbsp&nbsp&nbsp&nbsp
+        <a href="https://rubictronsandakalum.blogspot.com">
+            <i class="fab fa-blogger-b"></i>&nbsp www.rubictronsandakalum.blogspot.com
+        </a>&nbsp&nbsp&nbsp&nbsp&nbsp
+        <a href="https://github.com/rubictron?tab=repositories">
+            <i class="fab fa-github-square"></i>&nbsp Git hub(rubictron)
+        </a>
+    </div>
+
+
+    </div>
+
+</footer>
 </body>
 
 
